34db443e2114ad3243056d3dbbc519c2a892c51f
WilliamZapataMolina <154532291+WilliamZapataMolina@users.noreply.github.com>
2025-08-19 12:22:40 +0200

    improvement of the chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,12 +5,34 @@ function startChat(socket, roomId) {
     const chatForm = document.getElementById('chatForm');
     const chatInput = document.getElementById('chatInput');
     const chatMessages = document.getElementById('chatMessages');
+    const chatHeader = document.querySelector('.chat-header');
 
+    //Lógica para que la ventana sea arrastrable
+    let isDragging = false;
+    let offsetX, offsetY;
 
+    chatHeader.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        offsetX = e.clientX - chatModal.offsetLeft;
+        offsetY = e.clientY - chatModal.offsetTop;
+        chatModal.style.transition = 'none';
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        chatModal.style.left = `${e.clientX - offsetX}px`;
+        chatModal.style.top = `${e.clientY - offsetY}px`;
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+        chatModal.style.transition = 'transform 0.2s'; // Añade una transición suave al soltar
+    });
 
     //Muestra el modal del chat al hacer clcik en el ícono
     chatIcon.addEventListener('click', () => {
-        chatModal.classList.add('show');
+        chatModal.style.display = 'block';
+        chatIcon.classList.remove('has-new-messages');
 
         //Asegura que los mensajes se muestren desde el final
         chatMessages.scrollTop = chatMessages.scrollHeight;
@@ -18,7 +40,7 @@ function startChat(socket, roomId) {
 
     //Oculta el modal del chat al hacer click en el botón de cerrar
     closeChatBtn.addEventListener('click', () => {
-        chatModal.classList.remove('show');
+        chatModal.style.display = 'none';
     });
     //Envía el mensaje al servidor al enviar el formulario
     chatForm.addEventListener('submit', (e) => {
@@ -34,6 +56,10 @@ function startChat(socket, roomId) {
         const messageElement = document.createElement('div');
         messageElement.textContent = `${data.sender}: ${data.message}`;
         chatMessages.appendChild(messageElement);
+
+        if (chatModal.style.display === 'none') {
+            chatIcon.classList.add('has-new-messages'); // Muestra la notificación si el chat está cerrado
+        }
         // Desplazarse al último mensaje
         chatMessages.scrolltop = chatMessages.scrollHeight;
     });
